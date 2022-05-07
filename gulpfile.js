ab9f1855e4a60807fed349c688fe46080c3b248d
Marina Golacheva <marina.golacheva@gmail.com>
2022-05-07 19:40:47 +0200

    the third html&css project

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('styles', () => {
+    return gulp.src('scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('scss/**/*.scss', (done) => {
+        gulp.series(['styles'])(done);
+    });
+});
+
